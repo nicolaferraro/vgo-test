8b91cfe75b2582649baa0da95e6bee5a52055f82
nferraro <ni.ferraro@gmail.com>
2018-02-27 00:58:42 +0100

    test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module "github.com/nicolaferraro/vgo-test"
+
+require "rsc.io/quote" v1.5.2
---
+++ hello.go
@@ -0,0 +1,11 @@
+package main // import "github.com/nicolaferraro/vgo-test"
+
+import (
+	"fmt"
+	"rsc.io/quote"
+)
+
+func main() {
+	fmt.Println(quote.Hello())
+}
+
